0374542075570dd413226baaa95b1a634ea45e01
danielnachumdev-hplaptop <danielnachumdev@gmail.com>
2022-07-26 13:12:16 +0300

    pre matrix refactor

--- src/la2/BilinearForm.py
+++ src/la2/BilinearForm.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Callable, Union
-from ..la1 import Complex, Vector, Field, Matrix
+from ..la1 import Complex, Vector, Field, Matrix, VectorSpace
 
 
 class BilinearForm:
@@ -20,14 +20,13 @@ class BilinearForm:
     def fromMatrix(mat: Matrix) -> BilinearForm:
         pass
 
-    def __init__(self, func: Callable[[Vector, Vector], Union[int, float, Complex]], field: Field) -> None:
+    def __init__(self, func: Callable[[Vector, Vector], Union[int, float, Complex]], vector_space: VectorSpace) -> None:
         self.func = func
-        self.field = field
+        self.vector_space = vector_space
 
     @property
     def kernel(self):
-        mat = self.toMatrix()
-        return mat.kernel
+        return self.toMatrix().kernel
 
     @property
     def isSymmetrical(self) -> bool:
@@ -45,7 +44,12 @@ class BilinearForm:
         return self.func(v, u)
 
     def toMatrix(self) -> Matrix:
-        pass
+        basis = self.vector_space.standard_basis()
+        mat = [[0 for i in range(len(basis))] for j in range(len(basis))]
+        for i, v in enumerate(basis):
+            for j, u in enumerate(basis):
+                mat[i][j] = self.func(v, u)
+        return Matrix(mat, field=self.vector_space.field)
 
     def arePerpendicular(self, v1: Vector, v2: Vector) -> bool:
         """
@@ -55,10 +59,12 @@ class BilinearForm:
 
 
 class SquareBilinearForm(BilinearForm):
-
     @property
     def isSquare(self) -> bool:
         return True
 
     def __call__(self, v):
         return self.func(v, v)
+
+    def toMatrix(self) -> Matrix:
+        pass
--- src/la2/InnerProductSpace.py
+++ src/la2/InnerProductSpace.py
@@ -1,10 +1,22 @@
 from __future__ import annotations
-from ..la1 import VectorSpace
+from typing import Union
+from ..utils import areinstances
+from ..la1 import VectorSpace, Field, Complex, Vector
 from .InnerProduct import InnerProduct
 
 
-class InnerProductSpace:
-    def __init__(self, V: VectorSpace, IP: InnerProduct) -> None:
-        pass
+class InnerProductSpace(VectorSpace):
+    def __init__(self, field: Field, inner_product: InnerProduct) -> None:
+        super().__init__(field)
+        self.inner_product = inner_product
 
-    # def __call__(self,v1,v2) || def calculate(self,v1,v2)?
+    def apply(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        return self(v, u)
+
+    def inner_product(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        return self(v, u)
+
+    def __call__(self, v: Vector, u: Vector) -> Union[int, float, Complex]:
+        if not areinstances([v, u], Vector):
+            raise TypeError("v and u must be of type Vector")
+        return self.inner_product(v, u)
